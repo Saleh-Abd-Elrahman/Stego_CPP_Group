e5986aa688fa5bba9ab0796f620259747d2abe60
farah orfaly <farah.orfaly@gmail.com>
2024-12-03 11:57:45 +0100

    Update cmake and header

--- include/encode.h
+++ include/encode.h
@@ -4,7 +4,7 @@
 #include <string>
 
 // Function declarations
-std::string encodeMessage(const std::string& targetFilePath, const std::string& message);
-std::string encodeText(const std::string& targetFilePath, const std::string& textFilePath);
+std::string encodeMessage(const std::string& targetFilePath, const std::string& message, const std::string& password);
+std::string encodeText(const std::string& targetFilePath, const std::string& textFilePath, const std::string& password);
 
 #endif // ENCODE_H
