82aa8fd5ea56e42017e5b48652f89753fd775de5
farah orfaly <farah.orfaly@gmail.com>
2024-12-03 12:02:56 +0100

    Revert "Merge branch 'main' into bashencode"
    This reverts commit 0b9706d37a318e44db4dd549a6496e6aa0024eba, reversing
    changes made to 4482ff7ca6d99cc3327df9569e60083998bd6016.

--- include/decode.h
+++
@@ -1,9 +0,0 @@
-#ifndef DECODE_H
-#define DECODE_H
-
-#include <string>
-
-// Function declaration
-std::string decode(const std::string& filePath);
-
-#endif // DECODE_H
--- src/decode.cpp
+++
@@ -1,7 +0,0 @@
-#include "decode.h"
-
-// Decode the file and return the hidden message
-std::string decode(const std::string& filePath) {
-    // Implementation for decoding
-    return "Decoded message from: " + filePath;
-}
