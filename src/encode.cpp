152b62474769f3f70e134e3b35c047eb28179832
farah orfaly <farah.orfaly@gmail.com>
2024-12-03 11:21:41 +0100

    updating functions for password encoding

--- src/encode.cpp
+++ src/encode.cpp
@@ -4,13 +4,14 @@
 #include <stdexcept>
 
 // Encode a message
-std::string encodeMessage(const std::string& targetFilePath, const std::string& message) {
+std::string encodeMessage(const std::string& targetFilePath, const std::string& message, const std::string& password) {
+
     // Implementation for encoding a message
     return "Encoded message into: " + targetFilePath;
 }
 
 // Encode the content of a text file
-std::string encodeText(const std::string& targetFilePath, const std::string& textFilePath) {
+std::string encodeText(const std::string& targetFilePath, const std::string& textFilePath, const std::string& password) {
     std::ifstream file(textFilePath);
     if (!file.is_open()) {
         throw std::runtime_error("Failed to open the text file: " + textFilePath);
